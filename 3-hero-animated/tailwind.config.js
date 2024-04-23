9330e0447ca88f6c69503eb740bd3ed4b9e4cf85
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-04-23 11:59:36 +0530

    background-image set

---
+++ 3-hero-animated/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#1fb6ff",
+        purple: "#7e5bef",
+        pink: "#ff49db",
+        orange: "#ff7849",
+        green: "#13ce66",
+        yellow: "#ffc82c",
+        "gray-dark": "#273444",
+        gray: "#8492a6",
+        "gray-light": "#d3dce6",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        lato: ["Lato", "sans-serif"],
+        dosis: ["Dosis", "sans-serif"],
+      },
+      backgroundImage: {
+        hero: "url('images/hero.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
